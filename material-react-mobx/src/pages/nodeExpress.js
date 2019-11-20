2144ee7f14a0aef304cb7ca7c4d8c8566e613fca
박대규 <mamma1234@gmail.com>
2019-11-20 19:13:58 +0900

    first commit

--- material-react-mobx/src/pages/nodeExpress.js
+++ material-react-mobx/src/pages/nodeExpress.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 class nodeExpress extends Component {
     state = {
@@ -21,13 +28,27 @@ class nodeExpress extends Component {
         return (
             <div>
                 <h3>Express Test</h3>
-
-                {this.state.posts.map(item => (
-                    <div key={item.id}>
-                        <h1>{item.name}</h1>
-                        <span>{item.job}</span>
-                    </div>
-                ))}
+<Paper>                
+<Table>
+<TableHead>
+<TableRow>
+<TableCell>번호</TableCell>
+<TableCell>이미지</TableCell>
+<TableCell>이름</TableCell>
+<TableCell>생년월일</TableCell>
+<TableCell>성별</TableCell>
+<TableCell>직업</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{this.state.posts ? this.state.posts.map(c => {
+return <TableRow><TableCell>{c.id}</TableCell><TableCell>{c.image}</TableCell>
+<TableCell>{c.name}</TableCell><TableCell>{c.birthday}</TableCell>
+<TableCell>{c.gender}</TableCell><TableCell>{c.job}</TableCell></TableRow>
+}) : ''}
+</TableBody>
+</Table>
+</Paper>
             </div>
         );
     }
